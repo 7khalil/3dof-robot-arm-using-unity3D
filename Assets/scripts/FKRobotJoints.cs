aebfd86dd6634a533a8a11db8e0ab4650dbb33cd
7khalil <meftah.khalil@live.fr>
2020-02-06 14:33:44 +0100

    forward and inverse kinematics

---
+++ Assets/scripts/FKRobotJoints.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FKRobotJoints : MonoBehaviour 
+{
+    public Transform[] Joints;
+    public Transform endEffector;
+
+    public Slider S_Slider;    //PositionX min=4 max=12
+    public Slider L_Slider;    //PositionY min=-4 max=4
+    public Slider U_Slider;    //PositionZ min=4 max=12
+
+    public static double[] theta = new double[3];
+
+    private float a2, a3;
+
+    public float px, py, pz;
+
+    void Start()
+    {
+        theta[0] = theta[1] = theta[2] = 0.0;
+        a2 = 4.0f;
+        a3 = 4.0f;
+
+        px = 7f; py = 0f; pz = 7f;
+    }
+
+    void Update()
+    {    
+        theta[0] = S_Slider.value;
+        theta[1] = L_Slider.value;
+        theta[2] = U_Slider.value;
+
+        if (!double.IsNaN(theta[0]))
+            Joints[0].transform.localEulerAngles = new Vector3(0, (float)theta[0], 0);
+        if (!double.IsNaN(theta[2]))
+            Joints[2].transform.localEulerAngles = new Vector3((float)theta[2], 0, 0);
+        if (!double.IsNaN(theta[1]))
+            Joints[1].transform.localEulerAngles = new Vector3((float)theta[1], 0, 0);
+
+        Debug.Log("END EFFECTOR POSITION = " + endEffector.transform.position);
+    }
+}
--- Assets/scripts/RobotJoints.cs
+++ Assets/scripts/RobotJoints.cs
@@ -1,35 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class RobotJoints : MonoBehaviour {
-	
-    public float speed = 5.0f;
+public class RobotJoints : MonoBehaviour 
+{
+    public Transform[] Joints;
+    public Transform endEffector;
 
-    private float pivot0Slider = 0.0f;
-    private float pivot1Slider = 0.0f;
-    private float pivot2Slider = 0.0f;
-    
-    public GameObject pivot1, pivot2;
+    public Slider S_Slider;    //PositionX min=4 max=12
+    public Slider L_Slider;    //PositionY min=-4 max=4
+    public Slider U_Slider;    //PositionZ min=4 max=12
 
+    public static double[] theta = new double[3];
 
-     void OnGUI()
+    private float a2, a3;
+    private float D;
+
+    public float px, py, pz;
+
+    void Start()
     {
-        pivot0Slider = GUI.HorizontalSlider(new Rect (25, 25, 100, 30), pivot0Slider, -5.0f, 5.0f);
-        pivot1Slider = GUI.HorizontalSlider(new Rect (25, 50, 100, 30), pivot1Slider, -5.0f, 5.0f);
-        pivot2Slider = GUI.HorizontalSlider(new Rect (25, 75, 100, 30), pivot2Slider, -5.0f, 5.0f);
+        theta[0] = theta[1] = theta[2] = 0.0;
+        a2 = 4.0f;
+        a3 = 4.0f;
+
+        px = 7f; py = 0f; pz = 7f;
     }
 
-    void Update () {
+    void Update()
+    {    
+        pz = S_Slider.value;
+        py = L_Slider.value;
+        px = U_Slider.value;
 
-        transform.Rotate(0, pivot0Slider*speed*Time.deltaTime, 0);
-        pivot1.transform.Rotate(pivot1Slider*speed*Time.deltaTime, 0, 0);
-        pivot2.transform.Rotate(pivot2Slider*speed*Time.deltaTime, 0, 0);
+        px = px + 0.01f;
 
-        if (Input.GetMouseButtonUp(0)){
-                pivot0Slider = 0;
-                pivot1Slider = 0;
-                pivot2Slider = 0;
-        }
+        D = Mathf.Pow(px, 2) + Mathf.Pow(py, 2) + Mathf.Pow(pz, 2) - (Mathf.Pow(a2, 2) + Mathf.Pow(a3, 2));
+
+        theta[0] = Mathf.Atan2(pz, px);
+
+        theta[2] = Mathf.Acos(D/(2*a2*a3));
+
+        theta[1] = Mathf.Atan2(py, Mathf.Sqrt(Mathf.Pow(px, 2) + Mathf.Pow(pz, 2))) - Mathf.Atan2(a3*Mathf.Sin((float)theta[2]), a2+a3*Mathf.Cos((float)theta[2]));
+
+        if (!double.IsNaN(theta[0]))
+            Joints[0].transform.localEulerAngles = new Vector3(0, (float)theta[0] * Mathf.Rad2Deg + 180, 0);
+        if (!double.IsNaN(theta[2]))
+            Joints[2].transform.localEulerAngles = new Vector3((float)theta[2] * Mathf.Rad2Deg, 0, 0);
+        if (!double.IsNaN(theta[1]))
+            Joints[1].transform.localEulerAngles = new Vector3((float)theta[1] * Mathf.Rad2Deg - 90, 0, 0);
+
+        Debug.Log("END EFFECTOR POSITION = " + endEffector.transform.position);
     }
 }
+
